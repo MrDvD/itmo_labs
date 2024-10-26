a9eed8d27c36628d4147af52c6bae1707be8a0b5
MrDvD <DVDRUSSA@yandex.com>
2024-10-26 19:17:59 +0300

    Updated OPD Lab #1

--- opd/lab1/genresign.sh
+++ opd/lab1/genresign.sh
@@ -39,12 +39,14 @@ while getopts "hn:s:p:a:g:f:t:e:" arg; do
 done
 
 # validating input info
-test -z "$(grep -iE "^[A-ZА-ЯЁ]+{2,}$" <(echo $NAME))" && echo "WARNING: Missing a valid name option (-n)" >&2 && INVALID_FIELD=1
-test -z "$(grep -iE "^[A-ZА-ЯЁ]+{2,}$" <(echo $SURNAME))" && echo "WARNING: Missing a valid surname option (-s)" >&2 && INVALID_FIELD=1
+test -z "$(grep -iE "^[A-ZА-ЯЁ]{2,}$" <(echo $NAME))" && echo "WARNING: Missing a valid name option (-n)" >&2 && INVALID_FIELD=1
+test -z "$(grep -iE "^[A-ZА-ЯЁ]{2,}$" <(echo $SURNAME))" && echo "WARNING: Missing a valid surname option (-s)" >&2 && INVALID_FIELD=1
 test -z "$(grep -E "^[A-Z][0-9]{4,4}$" <(echo $GROUP))" && echo "WARNING: Missing a valid group option (-g)" >&2 && INVALID_FIELD=1
 test -z "$(grep -iE "^([A-ZА-ЯЁ]+ ?)+$" <(echo $FACULTY))" && echo "WARNING: Missing a valid faculty option (-f)" >&2 && INVALID_FIELD=1
 test -z "$(grep -E "^\+[0-9]{11,11}$" <(echo $PHONE))" -a -z "$(grep -E "^[A-Za-z.]+@[a-z]+\.[a-z]+$" <(echo $EMAIL))" && echo "WARNING: Missing either a valid phone number or a valid email options (-t or -e)" >&2 && INVALID_FIELD=1
 test -n "$INVALID_FIELD" && echo "ERROR: Not enough valid data for document creation. Exiting" && exit 1
 
 # generating man page
-man <(cat pattern.txt | sed -e "{s/SURNAME/$SURNAME/}" -e "{s/NAME/$NAME/}" -e "{s/PATRONYMIC/$PATRONYMIC/}" -e "{s/GROUP/$GROUP/}" -e "{s/FACULTY/$FACULTY/}" -e "{s/PHONE/${PHONE:--}/}" -e "{s/EMAIL/${EMAIL:--}/}" -e "s/DATE/$(date +"%d.%m.%Y")/")
+cat pattern.txt | sed -e s/SURNAME/"$SURNAME"/ -e s/NAME/"$NAME"/ -e s/PATRONYMIC/"$PATRONYMIC"/ -e s/GROUP/"$GROUP"/ -e s/FACULTY/"$FACULTY"/ -e s/PHONE/"${PHONE:--}"/ -e s/EMAIL/"${EMAIL:--}"/ -e s/DATE/$(date +"%d.%m.%Y")/ > file.tmp
+man ./file.tmp
+rm file.tmp
