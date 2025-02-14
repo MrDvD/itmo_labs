aaf824372259904dfff4273f53171febff88a33e
MrDvD <ma.labin@yandex.ru>
2025-02-14 22:12:30 +0300

    feat: started working on Main class

--- programming/5/src/main/java/com/itmo/mrdvd/Main.java
+++ programming/5/src/main/java/com/itmo/mrdvd/Main.java
@@ -1,8 +1,30 @@
 package com.itmo.mrdvd;
 
-/** Hello world! */
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
+  public static void main(String[] args) throws FileNotFoundException, IOException {
+    String envVar = "CONSOLE_CPATH";
+    String collectionPath = System.getenv(envVar);
+    if (collectionPath == null) {
+        System.out.format("[ERROR] Environment variable '%s' does not exist.\n", envVar);
+    } else {
+        if (new File(collectionPath).isFile()) {
+            FileInputStream file = new FileInputStream(collectionPath);
+            BufferedInputStream in = new BufferedInputStream(file);
+            int ch = in.read();
+            while (ch != -1) {
+                System.out.print((char) ch);
+                ch = in.read();
+            }
+            in.close();
+        } else {
+            System.out.format("[ERROR] File '%s' does not exist.\n", collectionPath); // what's with permissions?   
+        }
+    }
   }
 }
