2b5b2a2a84bc39c796a77772a07e19b6e441899e
MrDvD <ma.labin@yandex.ru>
2025-03-12 21:25:28 +0300

    fix: moved shell commands init to TicketShell's public method

--- programming/5/src/main/java/com/itmo/mrdvd/shell/TicketShell.java
+++ programming/5/src/main/java/com/itmo/mrdvd/shell/TicketShell.java
@@ -6,10 +6,32 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import com.itmo.mrdvd.collection.TicketCollection;
+import com.itmo.mrdvd.command.AddCommand;
+import com.itmo.mrdvd.command.AddIfMaxCommand;
+import com.itmo.mrdvd.command.ClearCommand;
 import com.itmo.mrdvd.command.Command;
+import com.itmo.mrdvd.command.CountGreaterThanEventCommand;
+import com.itmo.mrdvd.command.ExecuteScriptCommand;
+import com.itmo.mrdvd.command.ExitCommand;
+import com.itmo.mrdvd.command.HelpCommand;
+import com.itmo.mrdvd.command.InfoCommand;
+import com.itmo.mrdvd.command.LoadCommand;
+import com.itmo.mrdvd.command.MinByPriceCommand;
+import com.itmo.mrdvd.command.PrintFieldDescendingTypeCommand;
+import com.itmo.mrdvd.command.ReadEnvironmentFilepathCommand;
+import com.itmo.mrdvd.command.RemoveAtCommand;
+import com.itmo.mrdvd.command.RemoveByIdCommand;
+import com.itmo.mrdvd.command.RemoveLastCommand;
+import com.itmo.mrdvd.command.SaveCommand;
 import com.itmo.mrdvd.command.ShellInfo;
+import com.itmo.mrdvd.command.ShowCommand;
+import com.itmo.mrdvd.command.UpdateCommand;
+import com.itmo.mrdvd.device.Deserializer;
+import com.itmo.mrdvd.device.FileDescriptor;
 import com.itmo.mrdvd.device.InteractiveInputDevice;
 import com.itmo.mrdvd.device.OutputDevice;
+import com.itmo.mrdvd.device.Serializer;
 
 public class TicketShell extends Shell<Map<String, Command>, List<Command>> {
   private boolean isOpen;
@@ -52,6 +74,28 @@ public class TicketShell extends Shell<Map<String, Command>, List<Command>> {
     }
     getCommands().put(cmd.name(), cmd);
     return Optional.of(cmd);
+  }
+
+  public void initDefaultCommands(TicketCollection collection, FileDescriptor fd, Serializer<TicketCollection> serial, Deserializer<TicketCollection> deserial) {
+    addCommand(new AddCommand(collection, getInput(), getOutput()));
+    addCommand(new HelpCommand(getOutput()));
+    addCommand(new ExitCommand());
+    addCommand(new UpdateCommand(collection, getInput(), getOutput()));
+    addCommand(new ClearCommand(collection, getOutput()));
+    addCommand(new RemoveByIdCommand(collection, getOutput()));
+    addCommand(new RemoveAtCommand(collection, getOutput()));
+    addCommand(new RemoveLastCommand(collection, getOutput()));
+    addCommand(new ShowCommand(collection, getOutput()));
+    addCommand(new AddIfMaxCommand(collection, getInput(), getOutput()));
+    addCommand(new MinByPriceCommand(collection, getOutput()));
+    addCommand(new PrintFieldDescendingTypeCommand(collection, getOutput()));
+    addCommand(new CountGreaterThanEventCommand(collection, getOutput()));
+    addCommand(
+        new ReadEnvironmentFilepathCommand("TICKET_PATH", fd, getOutput()), true);
+    addCommand(new LoadCommand(fd, collection, deserial, getOutput()), true);
+    addCommand(new SaveCommand(collection, serial, fd, getOutput()));
+    addCommand(new ExecuteScriptCommand(getOutput(), fd));
+    addCommand(new InfoCommand(collection, getOutput()));
   }
 
   @Override
